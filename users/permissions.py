8c1cfe80b7ffe410bc4bb66a1f11be30673fe1b1
AndreyStyuhin <andreyst@olovyannaya.ru>
2025-10-14 17:13:04 +0900

    ready fix permissions.py

--- users/permissions.py
+++ users/permissions.py
@@ -4,17 +4,13 @@ from rest_framework import permissions
 class IsModer(permissions.BasePermission):
     """Разрешение, если пользователь входит в группу 'moderators'"""
 
-
-def has_permission(self, request, view):
-    user = request.user
-    return bool(user and user.is_authenticated and user.groups.filter(name='moderators').exists())
-
-
+    def has_permission(self, request, view):
+        user = request.user
+        return bool(user and user.is_authenticated and user.groups.filter(name='moderators').exists())
 
 
 class IsOwner(permissions.BasePermission):
     """Разрешение на основе наличия атрибута owner у объекта"""
 
-
-def has_object_permission(self, request, view, obj):
-    return bool(request.user and request.user.is_authenticated and getattr(obj, 'owner', None) == request.user)
+    def has_object_permission(self, request, view, obj):
+        return bool(request.user and request.user.is_authenticated and getattr(obj, 'owner', None) == request.user)
